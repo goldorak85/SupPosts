fd7ae42944b6604e9a2c84ad486a8fa34ba014e9
Maxence Pelletier <maxence.pelletier@epitech.eu>
2023-11-06 16:53:50 +0100

    Add post page

--- src/pages/Posts/_id.tsx
+++ src/pages/Posts/_id.tsx
@@ -1,14 +1,11 @@
 import '../../App.css'
 import Post from "../../components/Post";
-import { useParams } from 'react-router-dom';
 
 export default function PostById() {
-    const { id }: string = useParams();
 
     return (
         <>
             <div>
-                <h1>{id}</h1>
                 <Post author="MY POST SOLO" content="testtttt" id="tetete" like="0"/>
             </div>
         </>
