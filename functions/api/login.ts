6f0d9cd2e5ff36112d1d7a0da67fabb2c0904c8e
Maxence Pelletier <maxence.pelletier@epitech.eu>
2023-11-08 10:47:35 +0100

    Add debug

--- functions/api/login.ts
+++ functions/api/login.ts
@@ -5,7 +5,9 @@ import jwt from '@tsndr/cloudflare-worker-jwt';
 
 export async function onRequestPost({ request, env }) {
     const body: Login = await request.json();
+    console.log(JSON.stringify(body));
     const u = await env.users.get(body.email);
+    console.log(u);
     if (!u || u !== null || u !== undefined) {
         return new Response(JSON.stringify({error: 'User not found'}), {
             headers: {
@@ -15,14 +17,16 @@ export async function onRequestPost({ request, env }) {
         });
     }
     const user: User = JSON.parse(u);
+    console.log(JSON.stringify(user));
     if (!(await verify(body.password, body.email, user.password, env))) {
-        return new Response(JSON.stringify({error: 'User not found'}), {
+        return new Response(JSON.stringify({error: 'Password not good'}), {
             headers: {
                 'Content-Type': 'application/json'
             },
             status: 401
         });
     }
+    console.log("DEBUG");
     user.password = await jwt.sign({
         id: user.id,
         email: user.email,
@@ -30,6 +34,7 @@ export async function onRequestPost({ request, env }) {
         exp: Math.floor(Date.now() / 1000) + (5 * 60),
         iss: 'SUPDEVINCI-TP-DEV'
     }, env.secret);
+    console.log(JSON.stringify(user));
     return new Response(JSON.stringify(user), {
         headers: {
             'Content-Type': 'application/json'
